81a6629dfc5029c84f9cfedabefaf6f35cadfa92
1022jhui <2187447868@qq.com>
2019-01-03 09:23:04 +0800

    最后11

--- src/main/java/com/zking/erp/controller/cao/AccountCController.java
+++ src/main/java/com/zking/erp/controller/cao/AccountCController.java
@@ -275,6 +275,7 @@ public class AccountCController extends BaseController {
             Account.setRemark(model.getRemark());
             Account.setId(UUID.randomUUID().toString());
             accountService.insertAccount(Account);
+
             //========标识位===========
             flag = true;
             //记录操作日志使用
